9361a193814a7e7cbf1fc5a7f2b78192ab5f0423
Marcos Lopez <rippleytrigger@gmail.com>
2019-11-01 15:48:15 -0400

    Monster Slayer

--- js/app.js
+++ js/app.js
@@ -1,13 +1,73 @@
 new Vue({
-    el: '#app',
-    data: {
-      playerHealth: 100,
-      monsterHealth: 100,
-      startGame: false,
-      eventsLog: []
+  el: '#app',
+  data: {
+    playerHealth: 100,
+    monsterHealth: 100,
+    startGame: false,
+    eventsLog: [],
+    maxHealth: 100,
+    minHealth: 0,
+    maxBasicAttackDamage: 20,
+    maxSpecialAttackDamage: 50,
+    maxHealAmount: 20,
+    monsterAttackDamage: 20
+  },
+  watch: {
+    monsterHealth: function(){
+      if(this.monsterHealth <= this.minHealth){
+        alert("You've won the game!!!")
+        this.startGame = false;
+      }
     },
-    computed: {
-        
+    playerHealth: function(){
+      if(this.playerHealth <= this.minHealth){
+        alert("You've lost the game!!!")
+        this.startGame = false;
+      }
+    },
+    startGame: function(){
+      if(this.startGame == false){
+        this.resetData();
+      }
     }
-  })
+  },
+  computed: {
+    
+  },
+  methods: {
+    /**
+     * Player Methods
+     */
+    basicPlayerAttack: function(){
+      let playerAttackDamage = Math.floor(Math.random() * this.maxBasicAttackDamage) + 1;
+      this.monsterHealth -= playerAttackDamage;
+    },
+    specialPlayerAttack: function(){
+      let playerAttackDamage = Math.floor(Math.random() * this.maxSpecialAttackDamage) + 1;
+      this.monsterHealth -= playerAttackDamage;
+    },
+    healPlayer: function(){
+      let healPlayerLimit = this.maxHealth - this.playerHealth
+
+      if(healPlayerLimit < 20){
+        this.playerHealth += Math.floor(Math.random() * healPlayerLimit);
+      }else{
+        this.playerHealth += Math.floor(Math.random() * this.maxHealAmount);
+      }
+    },
+
+    /**
+     *  Monster Method
+     */
+    monsterAttack: function(){
+
+    },
+    resetData: function(){
+      this.playerHealth = this.maxHealth;
+      this.monsterHealth = this.maxHealth;
+    },
+  }
+})
+
+  
   
